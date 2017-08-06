f9cc996f18a4054af6a5f014eaf544d95ee3b520
Joel Hawksley <joel@hawksley.org>
2017-08-06 11:09:05 -0600

    [FIX] Lookup events by date in timezone

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   def calendar_events_for(date)
     calendar_events.select do |event|
       if event["start"].key?("date")
-        (DateTime.parse(event["start"]["date"])..DateTime.parse(event["end"]["date"])).cover?(date)
+        (DateTime.parse(event["start"]["date"])..DateTime.parse(event["end"]["date"])).cover?(date.in_time_zone("America/Denver"))
       else
         (event["start_i"]..event["end_i"]).overlaps?(date.in_time_zone("America/Denver").to_i..date.in_time_zone("America/Denver").end_of_day.to_i)
       end
