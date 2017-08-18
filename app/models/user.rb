189067702e9f6f77eda4cbe03713ac60d6b24b9a
Joel Hawksley <joel@hawksley.org>
2017-08-17 18:39:37 -0600

    extract WeatherService

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def fetch
+    update(weather: WeatherService.call(self), calendar_events: CalendarService.call(self))
+  end
+
   def calendar_events_for(beginning_i, ending_i)
     calendar_events.select do |event|
       (event["start_i"]..event["end_i"]).overlaps?(beginning_i...ending_i)
--- lib/tasks/fetch.rake
+++ lib/tasks/fetch.rake
@@ -1,8 +1,5 @@
 namespace :fetch do
   task all: :environment do
-    User.all.each do |user|
-      weather = HTTParty.get("http://api.wunderground.com/api/f79789b09f774c40/forecast/astronomy/conditions/q/#{user.location.delete(" ")}.json").as_json
-      user.update(weather: weather, calendar_events: CalendarService.call(user))
-    end
+    User.all.map(&:fetch)
   end
 end
