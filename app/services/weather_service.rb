8c4467ac26eaea6df2fea2d7a2f6ba6c91fc9df8
Joel Hawksley <joel@hawksley.org>
2018-01-07 10:41:57 -0700

    [design] weather for cait

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,5 +1,5 @@
 class WeatherService
   def self.call(user)
-    HTTParty.get("http://api.wunderground.com/api/f79789b09f774c40/forecast/astronomy/conditions/q/#{user.location.delete(" ")}.json").as_json
+    HTTParty.get("http://api.wunderground.com/api/f79789b09f774c40/forecast/astronomy/conditions/hourly/q/#{user.location.delete(" ")}.json").as_json
   end
 end
