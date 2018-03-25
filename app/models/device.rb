fac6c468d7252a4479a2d6e01188dd242bf906e2
Joel Hawksley <joel@hawksley.org>
2018-03-24 21:41:04 -0600

    do not include image stylesheets in stack

--- app/models/device.rb
+++ app/models/device.rb
@@ -14,10 +14,10 @@ class Device < ApplicationRecord
     html = Slim::Template.new(Rails.root.join("app", "views", "image_templates", "#{template}.html.slim")).render(Object.new, view_object: user.render_json_payload)
 
     image = IMGKit.new(html, imgkit_params)
-    image.stylesheets << Rails.root.join("app", "assets", "stylesheets", "image_templates", "font-awesome.css")
-    image.stylesheets << Rails.root.join("app", "assets", "stylesheets", "image_templates", "weathericons.css")
-    image.stylesheets << Rails.root.join("app", "assets", "stylesheets", "image_templates", "fonts.css")
-    image.stylesheets << Rails.root.join("app", "assets", "stylesheets", "image_templates", "styles.css")
+    image.stylesheets << Rails.root.join("app", "assets", "image_templates", "font-awesome.css")
+    image.stylesheets << Rails.root.join("app", "assets", "image_templates", "weathericons.css")
+    image.stylesheets << Rails.root.join("app", "assets", "image_templates", "fonts.css")
+    image.stylesheets << Rails.root.join("app", "assets", "image_templates", "styles.css")
 
     update(current_image: image.to_img(:png))
   end
