438d63fe11c5a707637eea2919b2babec7cbdda6
Joel Hawksley <joel@hawksley.org>
2019-01-21 15:31:23 -0700

    add lat/long for darksky compatibility

---
+++ db/migrate/20190121222623_add_lat_long_to_users.rb
@@ -0,0 +1,6 @@
+class AddLatLongToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :latitude, :decimal, { precision: 10, scale: 6 }
+    add_column :users, :longitude, :decimal, { precision: 10, scale: 6 }
+  end
+end
