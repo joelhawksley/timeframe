43009b8dd08d832f3839a2540d4c4d82c98de825
Joel Hawksley <joel@hawksley.org>
2021-05-31 17:10:06 -0600

    add basic test harness

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require "minitest/autorun"
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending!
