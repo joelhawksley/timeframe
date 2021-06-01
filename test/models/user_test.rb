fefbdcbe28e046e6949e02fec32ef82a99b013f5
Joel Hawksley <joel@hawksley.org>
2021-05-31 19:10:12 -0600

    add tests for User#alerts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,23 @@ class UserTest < Minitest::Test
   def test_hardcoded_tz
     assert_equal(User.new.tz, "America/Denver")
   end
+
+  def test_alerts_empty_state
+    assert_equal([], User.new(weather: {}).alerts)
+  end
+
+  def test_alerts_with_error_message
+    assert_equal(["foo"], User.new(weather: {}, error_messages: ["foo"]).alerts)
+  end
+
+  def test_alerts_with_weather_alert
+    assert_equal(["bar"], User.new(weather: {alerts: [{title: "bar"}]}).alerts)
+  end
+
+  def test_alerts_with_weather_alert_and_error_message
+    assert_equal(
+      ["foo", "bar"],
+      User.new(weather: {alerts: [{title: "bar"}]}, error_messages: ["foo"]).alerts
+    )
+  end
 end
