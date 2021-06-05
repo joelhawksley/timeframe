04307df51c71ccab3bc93de5abc416d020a0a89b
Joel Hawksley <joel@hawksley.org>
2021-06-05 17:07:04 -0600

    add basic Device tests

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -10,4 +10,16 @@ class DeviceTest < Minitest::Test
   def test_height
     assert_equal(1200, Device.new(template: "13_calendar_weather").height)
   end
+
+  def test_battery_level_default
+    assert_equal(100, Device.new.battery_level)
+  end
+
+  def test_battery_level_invalid_hash
+    assert_equal(100, Device.new(status: {}).battery_level)
+  end
+
+  def test_battery_level_valid_hash
+    assert_equal(93, Device.new(status: { "Status" => { "Battery" => 93.09 } }).battery_level)
+  end
 end
