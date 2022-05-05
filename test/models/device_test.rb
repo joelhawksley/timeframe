e34e588c7d93e12a2683ab2ffc1e0022c91a7384
Joel Hawksley <joel@hawksley.org>
2022-05-04 21:29:44 -0600

    fix test

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -7,11 +7,11 @@ class DeviceTest < Minitest::Test
   include Capybara::Minitest::Assertions
 
   def test_width
-    assert_equal(1600, Device.new(template: "13_calendar_weather").width)
+    assert_equal(1200, Device.new(template: "13_calendar_weather").width)
   end
 
   def test_height
-    assert_equal(1200, Device.new(template: "13_calendar_weather").height)
+    assert_equal(1600, Device.new(template: "13_calendar_weather").height)
   end
 
   def test_battery_level_default
