9d0dde2f9410c9298148acf3bcc1b6e8dec9f282
Joel Hawksley <joel@hawksley-server.local>
2022-11-30 15:59:35 -0700

    add fetched at, hardcode lat/long, etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,4 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
-  def update
-    current_user.update(user_params)
-
-    redirect_to(root_path, flash: {notice: "Updated"})
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:latitude, :longitude)
-  end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -3,7 +3,7 @@
 class WeatherService
   def self.call(user)
     user.update(weather:
-      HTTParty.get("https://api.darksky.net/forecast/#{ENV["DARK_SKY_API_KEY"]}/#{user.latitude},#{user.longitude}?extend=hourly").as_json)
+      HTTParty.get("https://api.darksky.net/forecast/#{ENV["DARK_SKY_API_KEY"]}/39.9147082,-105.0220883?extend=hourly").as_json)
   rescue => e
     user.update(error_messages: user.error_messages << e.message)
   end
