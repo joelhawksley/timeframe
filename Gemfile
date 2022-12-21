34b86a9fbe4734592f7584c02a3ee792685d7b65
Joel Hawksley <joel@hawksley-server.local>
2022-12-21 15:17:04 -0700

    remove devise

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ source "https://rubygems.org"
 ruby "3.0.3"
 
 gem "bootstrap-sass"
-gem "devise"
 gem "dotenv-rails"
 gem "font-awesome-sass", "~> 6.2.1"
 gem "google-api-client", "~> 0.11", require: ["google/apis/calendar_v3", "google/apis/people_v1", "google/apis/gmail_v1"]
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   get :thirteen, to: "templates#thirteen"
   get :mira, to: "templates#mira"
-  get :twenty_five, to: "templates#twenty_five"
   get :redirect, to: "home#redirect"
   get :google_callback, to: "home#callback"
   resources :templates, only: [:show]
