8f08be55358d82ad6fad7ed1b629eb0c1167fff2
Joel Hawksley <joel@hawksley-server.local>
2022-12-21 15:20:25 -0700

    remove users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_220218) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_221757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "google_accounts", force: :cascade do |t|
-    t.bigint "user_id"
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_220218) do
     t.boolean "email_enabled", default: false, null: false
     t.jsonb "emails", default: [], null: false
     t.index ["email"], name: "index_google_accounts_on_email"
-    t.index ["user_id"], name: "index_google_accounts_on_user_id"
   end
 
   create_table "google_calendars", force: :cascade do |t|
@@ -44,23 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_220218) do
     t.string "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "values", force: :cascade do |t|
