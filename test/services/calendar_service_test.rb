379be67991f08d4b456a0df7c35b8625b5b3ad8b
joel <joel@Joels-MBP.localdomain>
2023-08-01 13:35:58 -0600

    add test for result with remainder

--- test/services/calendar_service_test.rb
+++ test/services/calendar_service_test.rb
@@ -9,7 +9,13 @@ class CalendarServiceTest < Minitest::Test
     assert_equal("1w", result)
   end
 
-  def test_baby_age_string
+  def test_baby_age_string_weeks_days
+    result = CalendarService.baby_age_string(Date.today - 8.days)
+
+    assert_equal("1w1d", result)
+  end
+
+  def test_baby_age_string_less_than_one_week
     result = CalendarService.baby_age_string(Date.today - 6.days)
 
     assert_equal("6d", result)
