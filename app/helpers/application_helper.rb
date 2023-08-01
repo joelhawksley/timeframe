69a62df4061bf0a13f2ce1b65382e1f25a6b8612
joel <joel@Joels-MBP.localdomain>
2023-08-01 11:59:22 -0600

    remove flash_class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def flash_class(level)
-    case level
-    when :success then 'alert alert-success'
-    when :error then 'alert alert-error'
-    when :alert then 'alert alert-error'
-    else 'alert alert-info'
-    end
-  end
-
   def baby_age_string
     day_count = Date.today - Date.parse(ENV['BIRTH_DATE'])
     week_count = (day_count / 7).to_i
