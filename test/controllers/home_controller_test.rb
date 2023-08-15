cab8cf4ff3aea195d528b422f528c169d7c37e32
joel <joel@Joels-MBP.localdomain>
2023-08-15 08:02:36 -0600

    add basic integration test for #thirteen

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,8 +3,17 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get #mira" do
+  test "should get #mira with no data" do
     get "/mira"
+
+    # look for tomorrow's day name, as current day is not always shown
+    assert_includes response.body, Date.tomorrow.strftime("%A")
+
+    assert_response :success
+  end
+
+  test "should get #thirteen with no data" do
+    get "/thirteen"
 
     # look for tomorrow's day name, as current day is not always shown
     assert_includes response.body, Date.tomorrow.strftime("%A")
