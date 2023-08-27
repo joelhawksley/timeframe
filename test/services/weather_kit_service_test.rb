1b5f4fa06d4f85c6695c51ac3d3d37a5d63699c1
joel <joel@Joels-MBP.localdomain>
2023-08-27 15:42:45 -0600

    add test for temperature_range_for

--- test/services/weather_kit_service_test.rb
+++ test/services/weather_kit_service_test.rb
@@ -11,6 +11,29 @@ class WeatherKitServiceTest < Minitest::Test
     assert_nil(WeatherKitService.temperature_range_for(Date.today))
   end
 
+  def test_temperature_range_for_example
+    weather = {
+      "forecastDaily" => {
+        "days" => [
+          {
+            "forecastStart"=>"2023-08-27T06:00:00Z",
+            "temperatureMax"=>30.38,
+            "temperatureMin"=>14.47,
+          },
+          {
+            "forecastStart"=>"2023-08-28T06:00:00Z",
+            "temperatureMax"=>25.32,
+            "temperatureMin"=>15.16,
+          }
+        ]
+      }
+    }
+
+    WeatherKitService.stub :weather, weather do
+      assert_equal("&#8593;87 &#8595;58", WeatherKitService.temperature_range_for(Date.new(2023,8,27)))
+    end
+  end
+
   def test_current_temperature_no_data
     assert_nil(WeatherKitService.current_temperature)
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ SimpleCov.start
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "minitest/autorun"
+require "minitest/unit"
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
