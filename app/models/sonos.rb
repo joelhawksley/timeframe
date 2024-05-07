4efcd28018933bb42ea374bae2b507983405f51c
Joel Hawksley <joelhawksley@github.com>
2024-05-06 20:55:53 -0600

    Sonos is an ApiModel

--- app/models/sonos.rb
+++ app/models/sonos.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
-class Sonos
-  def self.healthy?
-    return false unless last_fetched_at
-
-    DateTime.parse(last_fetched_at) > DateTime.now - 1.minute
-  end
-
-  def self.data
-    MemoryValue.get(:sonos)[:data] || {}
-  end
-
-  def self.last_fetched_at
-    MemoryValue.get(:sonos)[:last_fetched_at]
+class Sonos < ApiModel
+  def self.time_before_unhealthy
+    1.minute
   end
 
   def self.status
@@ -55,17 +45,5 @@ class Sonos
         track: data["currentTrack"]["title"]
       }
     end
-  end
-
-  def self.fetch
-    response = HTTParty.get(Timeframe::Application.config.local["sonos_url"])
-
-    return if response["status"] == "error"
-
-    MemoryValue.upsert(:sonos,
-      {
-        data: response,
-        last_fetched_at: Time.now.utc.in_time_zone(Timeframe::Application.config.local["timezone"]).to_s
-      })
   end
 end
