86f410cc402e2ddd21a2127b25d7797236016884
Joel Hawksley <joelhawksley@github.com>
2024-05-08 21:09:57 -0600

    fix reference to old var

--- test/models/calendar_feed_test.rb
+++ test/models/calendar_feed_test.rb
@@ -197,7 +197,7 @@ class CalendarFeedTest < Minitest::Test
         start_time_utc = DateTime.new(2023, 8, 28, 20, 20, 0, "-0600").utc.to_time
         end_time_utc = DateTime.new(2023, 8, 29, 0, 0, 0, "-0600").utc.to_time
 
-        assert(CalendarFeed.events_for(start_time_utc, end_time_utc)[:daily].length == 1)
+        assert(CalendarFeed.events_for(start_time_utc, end_time_utc)[:daily].length == 0)
       end
     end
   end
