04b41be6597be612168be8c1bf87a6a98d03d8f1
Joel Hawksley <joel@hawksley.org>
2024-06-07 09:05:43 -0600

    standardize controller error handling

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -2,7 +2,13 @@ class DisplaysController < ApplicationController
   layout "display"
 
   def thirteen
-    render "thirteen", locals: {view_object: DisplayContent.new.call}
+    # :nocov:
+    begin
+      render "thirteen", locals: {view_object: DisplayContent.new.call}
+    rescue => e
+      render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
+    end
+    # :nocov:
   end
 
   def mira
@@ -12,17 +18,6 @@ class DisplaysController < ApplicationController
     begin
       render "mira", locals: {view_object: DisplayContent.new.call}
     rescue => e
-      Rails.logger.error("listing #{Thread.list.count} threads:")
-      Thread.list.each_with_index do |t,i|
-         Rails.logger.error("---- thread #{i}: #{t.inspect}")
-         Rails.logger.error(t.backtrace.take(5))
-      end
-
-      stats = ActiveRecord::Base.connection_pool.stat
-      Rails.logger.error("Connection Pool Stats #{stats.inspect}")
-
-      Rails.logger.error("Render error: " + e.message + e.backtrace.join("\n"))
-
       render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
     end
     # :nocov:
