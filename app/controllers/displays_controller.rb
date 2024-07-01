cb2be1e1119b2f5ca832d171bc9ecd1b79872a27
Joel Hawksley <joel@hawksley.org>
2024-07-01 15:18:20 -0600

    fix refresh logic

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,25 +1,23 @@
+# :nocov:
 class DisplaysController < ApplicationController
   layout "display"
 
   def thirteen
-    # :nocov:
+    @refresh = params[:refresh] || ""
 
     render "thirteen", locals: {view_object: DisplayContent.new.call}
   rescue => e
     render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
-
-    # :nocov:
   end
 
   def mira
-    @refresh = true
+    @refresh = params[:refresh] || 2
 
-    # :nocov:
     begin
       render "mira", locals: {view_object: DisplayContent.new.call}
     rescue => e
       render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
     end
-    # :nocov:
   end
 end
+# :nocov:
