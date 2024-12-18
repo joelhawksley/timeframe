55ed368af2e1abed814d8459dfdb599bd7d6d0da
Joel Hawksley <joel@hawksley.org>
2024-12-18 11:12:06 -0700

    replace body instead of reloading entire page

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -3,18 +3,16 @@ class DisplaysController < ApplicationController
   layout "display"
 
   def thirteen
-    @refresh = params[:refresh] || ""
-
     render "thirteen", locals: {view_object: DisplayContent.new.call}
   rescue => e
     render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
   end
 
   def mira
-    @refresh = params[:refresh] || 2
+    @refresh = params[:refresh] != "false"
 
     begin
-      render "mira", locals: {view_object: DisplayContent.new.call}
+      render("mira", locals: {view_object: DisplayContent.new.call}, layout: params[:layout] != "false")
     rescue => e
       render "error", locals: {klass: e.class.to_s, message: e.message, backtrace: e.backtrace}
     end
