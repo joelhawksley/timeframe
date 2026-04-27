21a6d662d6a6fb0f223cc3cc68631366a28cb8be
Joel Hawksley <joelhawksley@github.com>
2026-04-27 14:21:36 -0600

    remove duplicate migration

--- engine/db/migrate/005_add_display_template_to_devices.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDisplayTemplateToDevices < ActiveRecord::Migration[8.1]
-  def change
-    add_column :devices, :display_template, :string, null: false, default: "default"
-  end
-end
